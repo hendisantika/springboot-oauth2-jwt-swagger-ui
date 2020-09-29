c1770ca5ba6d9810e58ef3d6d9a52d0800babe24
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-30 06:56:34 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-oauth2-jwt-swagger-ui
---
+++ src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/SpringBootOauth2JwtSwaggerUiApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootoauth2jwtswaggerui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2JwtSwaggerUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2JwtSwaggerUiApplication.class, args);
+    }
+
+}
