b64109531b9e3ec9a3f20318324723caa642bffc
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-30 07:13:16 +0700

    #14 - Add configure bean in ResourceServerConfiguration

--- src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/ResourceServerConfiguration.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/ResourceServerConfiguration.java
@@ -3,8 +3,11 @@ package com.hendisantika.springbootoauth2jwtswaggerui.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,4 +31,24 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
     @Value("${config.oauth2.resource.id}")
     private String resourceId;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .anonymous().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/oauth/**").authenticated();
+
+    }
+
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources
+                .resourceId(resourceId)
+                .tokenServices(tokenServices())
+                .tokenStore(tokenStore());
+    }
 }
