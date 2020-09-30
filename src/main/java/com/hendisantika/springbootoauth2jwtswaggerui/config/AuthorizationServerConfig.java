ee60d1512004d379f2e554a5982ceda4c0b7ab5e
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-30 07:07:28 +0700

    #6 - Add AuthorizationServerConfig

---
+++ src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/AuthorizationServerConfig.java
@@ -0,0 +1,49 @@
+package com.hendisantika.springbootoauth2jwtswaggerui.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-oauth2-jwt-swagger-ui
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 30/09/20
+ * Time: 07.07
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private static final Logger LOGGER = Logger.getLogger(AuthorizationServerConfig.class);
+
+    @Autowired
+    @Qualifier("userDetailsService")
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Value("${config.oauth2.tokenTimeout}")
+    private int expiration;
+
+    @Value("${config.oauth2.privateKey}")
+    private String privateKey;
+
+    @Value("${config.oauth2.publicKey}")
+    private String publicKey;
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+}
