1e96a7b4ec8d611ed64718b2a8baeab669a1f25a
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-30 07:17:37 +0700

    #19 - Add defaultAuth & security bean SwaggerConfig

--- src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/SwaggerConfig.java
@@ -5,17 +5,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.GrantType;
 import springfox.documentation.service.OAuth;
 import springfox.documentation.service.ResourceOwnerPasswordCredentialsGrant;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
@@ -82,6 +88,37 @@ public class SwaggerConfig {
     private SecurityContext securityContext() {
         return SecurityContext.builder().securityReferences(defaultAuth())
                 .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
+        authorizationScopes[0] = new AuthorizationScope("read", "read all");
+        authorizationScopes[1] = new AuthorizationScope("trust", "trust all");
+        authorizationScopes[2] = new AuthorizationScope("write", "write all");
+
+        return Collections.singletonList(new SecurityReference("oauth2", authorizationScopes));
+    }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return new SecurityConfiguration
+                ("client", "secret", "", "", "Bearer access token", ApiKeyVehicle.HEADER, HttpHeaders.AUTHORIZATION,
+                        "");
+    }
+
+
+    /**
+     * @return ApiInf
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Authentication API").description("")
+                .termsOfServiceUrl("https://www.example.com/api")
+                .contact(new Contact("Developers", "https://projects.spring.io/spring-boot/", ""))
+                .license("Open Source")
+                .licenseUrl("\"https://www.apache.org/licenses/LICENSE-2.0")
+                .version("1.0.0")
+                .build();
+
     }
 
 }
