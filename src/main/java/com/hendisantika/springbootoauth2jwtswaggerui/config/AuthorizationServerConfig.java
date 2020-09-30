a15676752ca39f83ad00e7054dd7ca2f83c379cf
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-30 07:08:35 +0700

    #7 - Add passwordEncoder & configure bean in AuthorizationServerConfig

--- src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/AuthorizationServerConfig.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/AuthorizationServerConfig.java
@@ -3,9 +3,13 @@ package com.hendisantika.springbootoauth2jwtswaggerui.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
@@ -46,4 +50,23 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private ClientDetailsService clientDetailsService;
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("client")
+                .authorizedGrantTypes("client_credentials", "password", "refresh_token", "authorization_code")
+                .scopes("read", "write")
+                .resourceIds("oauth2-resource")
+                .accessTokenValiditySeconds(expiration)
+                .refreshTokenValiditySeconds(expiration)
+                .secret("secret");
+
+    }
 }
