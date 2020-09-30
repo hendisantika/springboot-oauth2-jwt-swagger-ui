dea0f267992a43275315f1834d9a54a9c439db7f
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-30 07:06:22 +0700

    #5 - Add data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (user_id, username, password, enabled)
+VALUES ('1', 'user1@example.com', '$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri', TRUE),
+       ('2', 'user2@example.com', '$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri', TRUE),
+       ('3', 'user3@example.com', '$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri', TRUE);
