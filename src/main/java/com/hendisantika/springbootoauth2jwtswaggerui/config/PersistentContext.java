81a65bcdb251ad233253068095ac8edf31d72a9a
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-30 07:11:05 +0700

    #11 - Add dataSource & jpaVendorAdapter bean in PersistentContext

--- src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/PersistentContext.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/PersistentContext.java
@@ -1,8 +1,16 @@
 package com.hendisantika.springbootoauth2jwtswaggerui.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +25,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.hendisantika")
 @EnableJpaRepositories("com.hendisantika.repository")
 public class PersistentContext {
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
+        bean.setDatabase(Database.H2);
+        bean.setGenerateDdl(true);
+        return bean;
+    }
 }
