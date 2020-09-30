bf6712f1b8ae50a6eaf51321c26b8de1bab1f5bb
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-30 07:14:05 +0700

    #15 - Add tokenServices, accessTokenConverter & tokenStore bean in ResourceServerConfiguration

--- src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/ResourceServerConfiguration.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtswaggerui/config/ResourceServerConfiguration.java
@@ -2,12 +2,17 @@ package com.hendisantika.springbootoauth2jwtswaggerui.config;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * Created by IntelliJ IDEA.
@@ -50,5 +55,31 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
                 .resourceId(resourceId)
                 .tokenServices(tokenServices())
                 .tokenStore(tokenStore());
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        defaultTokenServices.setTokenEnhancer(accessTokenConverter());
+        return defaultTokenServices;
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+
+        log.info("Initializing JWT with public key: " + publicKey);
+
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(privateKey);
+
+        return converter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
     }
 }
